ca299b7603c19c11f8def662baaa4d14c55dd636
curryfizz <nawsheen141@gmail.com>
2023-06-25 22:00:59 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <button className="btn btn-secondary">Button</button>
+
+    </h1>
+  )
+}
